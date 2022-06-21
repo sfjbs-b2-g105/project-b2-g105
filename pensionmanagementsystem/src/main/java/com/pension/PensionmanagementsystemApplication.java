98420d43527b567d63b75ac729407e8b649dead6
sfjbs-b2-g105 <sfjbsolutionsb1g105@gmail.com>
2022-06-21 06:06:44 +0000

    sakshi commit

---
+++ pensionmanagementsystem/src/main/java/com/pension/PensionmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.pension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ pensionmanagementsystem/src/test/java/com/pension/demo/PensionmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pension.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
